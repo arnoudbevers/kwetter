dd51b8ea74e107b0cca8879809ad7ad0cde03c98
Arnoud Bevers <beversarnoud@gmail.com>
2019-05-18 13:46:47 +0200

    [TEMP] Started observable in kweet service
    Timeline needs to be observable to minimize API calls, started it but committing for home computer

--- kwetter-frontend/src/app/components/homepage-timeline/homepage-timeline.component.ts
+++ kwetter-frontend/src/app/components/homepage-timeline/homepage-timeline.component.ts
@@ -1,17 +1,35 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { User } from 'src/app/models/user';
+import { Component, OnInit, Input, OnDestroy } from "@angular/core";
+import { User } from "src/app/models/user";
+import { Kweet } from "src/app/models/kweet";
+import { KweetService } from "src/app/services/kweet/kweet.service";
+import { Observable, Subscription, BehaviorSubject } from "rxjs";
+import { StorageService } from "src/app/services/storage/storage.service";
 
 @Component({
-  selector: 'homepage-timeline',
-  templateUrl: './homepage-timeline.component.html',
-  styleUrls: ['./homepage-timeline.component.css']
+  selector: "homepage-timeline",
+  templateUrl: "./homepage-timeline.component.html",
+  styleUrls: ["./homepage-timeline.component.css"]
 })
 export class HomepageTimelineComponent implements OnInit {
   @Input() private currentUser: User;
+  // private _timeline: BehaviorSubject<Kweet[]> = new BehaviorSubject([]);
+  // // private readonly timeline =  this._timeline.asObservable();
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private kweetService: KweetService,
+    private storageService: StorageService
+  ) {
+    const timeline = new Observable((observer) => {
+      const {next, error} = observer;
+    })
   }
 
+  get timeline() {
+    // return this._timeline.asObservable();
+  }
+
+  ngOnInit() {}
+
+  loadInitialData() {
+  }
 }
--- kwetter-frontend/src/app/services/kweet/kweet.service.ts
+++ kwetter-frontend/src/app/services/kweet/kweet.service.ts
@@ -1,17 +1,46 @@
 import { Injectable } from "@angular/core";
 import { ApiService } from "../api/api.service";
 import { HttpClient } from "@angular/common/http";
-import { Kweet } from 'src/app/models/kweet';
+import { Kweet } from "src/app/models/kweet";
+import { map } from "rxjs/operators";
+import { BehaviorSubject, Observable, of, Subject } from "rxjs";
+import { StorageService } from "../storage/storage.service";
 
 @Injectable({
   providedIn: "root"
 })
 export class KweetService extends ApiService {
-  constructor(http: HttpClient) {
+  private subject = new Subject<any>();
+
+  private kweets = new Subject<Kweet[]>();
+
+  constructor(http: HttpClient, private storageService: StorageService) {
     super(http);
+    // this.dataStore = { timeline: [] };
+    // this._timeline = <BehaviorSubject<Kweet[]>>new BehaviorSubject([]);
+    // this.timeline = this._timeline.asObservable();
+  }
+
+  getAll(uuid: string) {
+    return this.getHttpClient().get(
+      `${this.getApiUrl()}/users/${uuid}/kweets`
+    );
+
+    // const kweetsObservable = new Observable(observer => {
+    //   setTimeout(() => {
+    //     observer.next(this.kweets);
+    //   }, 1000);
+    // })
+    // return kweetsObservable;
+    // return of(this.kweets);
   }
 
   postKweet(kweet: Kweet) {
-    return this.getHttpClient().post(`${this.getApiUrl()}/kweets`, kweet, this.getHttpOptions());
+    this.getHttpClient().post<Kweet>(
+      `${this.getApiUrl()}/kweets`,
+      kweet,
+      this.getHttpOptions()
+    );
+    this.subject.next(kweet);
   }
 }
